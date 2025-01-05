42900d3b0cdd966bcb0227a172034f36baf35cd4
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2025-01-05 00:28:57 -0700

    updates users service and adds findOne and Create functions

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { User, UserDocument } from './schemas/user.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async create(userDto: CreateUserDto): Promise<UserDocument | null> {
+    try {
+      const user = new this.userModel(userDto);
+
+      await user.save();
+
+      return user;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
   }
 
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  findOne(filter: object): Promise<UserDocument | undefined> {
+    return this.userModel.findOne(filter);
   }
 }
